0dc2e4a3abfb15a75c7c3b47e2c5b88e2938cbea
MostafaAboghida <mm@spacegene.com>
2017-02-26 17:18:14 +0200

    TestConnection

---
+++ src/testconnection1/TestConnection1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testconnection1;
+
+/**
+ *
+ * @author SpaceG7
+ */
+public class TestConnection1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("TTTTTTTTTTT");
+    }
+    public void printValue(){
+        System.out.println("yyyyyyyyyy");
+    }
+    
+}
